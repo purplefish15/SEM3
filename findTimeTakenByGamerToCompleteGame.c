8d730310dddec51f2f5867e9e137c821c8b8f5e2
Shweta Sidhwani <121174976+purplefish15@users.noreply.github.com>
2023-08-21 19:57:55 +0530

    Create findTimeTakenByGamerToCompleteGame.c

---
+++ findTimeTakenByGamerToCompleteGame.c
@@ -0,0 +1,40 @@
+/*No. of levels =X
+  Time taken for each level=Y
+  Time taken in the break=Z
+  NOTE-Breaks are taken after every 3 levels
+  TO FIND- Total time taken to complete the game. */
+
+#include<stdio.h>
+int timeFunction(int X, int Y, int Z)
+{
+    //Two variables: one for time taken in playing and another
+    //for time taken in breaks
+    int gamingTime=X*Y;
+    //Loop for breaks
+    int breaks=0;
+    while(X>3)
+    {
+        X=X-3;
+        breaks++;
+    }
+    int breakTime=breaks*Z;
+    int TOTAL=gamingTime+breakTime;
+    return(TOTAL);
+}
+void main(){
+    int T,X,Y,Z;
+    int TOT[100];
+    //Input no. of test-cases
+    scanf("\n%d",&T);
+    //Input X,Y,Z
+    int T1=T;
+    for(int i=0;i<T;i++)
+    {
+        scanf("\n%d %d %d",&X,&Y,&Z);
+        TOT[i]=timeFunction(X,Y,Z);
+    }
+    for(int i=0;i<T1;i++)
+    {
+        printf("\n%d",TOT[i]);
+    }
+}
