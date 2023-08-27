c9d8fbb543e3ca8aee6307441c95d3ce95b6c56f
Shweta Sidhwani <121174976+purplefish15@users.noreply.github.com>
2023-08-27 14:54:15 +0530

    Update stacksUsingArray.c

--- stacksUsingArray.c
+++ stacksUsingArray.c
@@ -1,73 +1,84 @@
 #include<stdio.h>
 #include<conio.h>
 #include<malloc.h>
+#include<stdlib.h>
 
-struct stack
-{
-    unsigned top;
-    unsigned MAX;
-    struct student *a;
-};
 struct student
 {
     int rollNo;
 };
 
+struct stack
+{
+    unsigned top;           //one memory location is wasted [0th location]
+    unsigned MAX;
+    struct student *a;      //on adding const, an error is thrown in push function, line 100
+};
+
+//function prototyping
+char isEmpty(struct stack *);
+char isFull(struct stack *);
+unsigned peek(struct stack *);
+void push(struct stack *, struct student);
+void pop(struct stack *);
+
 void main()
 {
+    int stchoice, num;
     struct stack *sptr;
     struct stack s1,s2;
     s1.top=s2.top=0;
     s1.a=s2.a=NULL;
-    int stchoice, num;
-    printf("\n Enter the maximum number of elements for s1: ");
-    scanf("%u",&s1.MAX);
-    printf("\n Enter the maximum number of elements for s2: ");
-    scanf("%u",&s2.MAX);
-    printf("\n Enter the choice of stack: (1 or 2)");
-    scanf("%d",&stchoice);
-    if(stchoice==1)
-    *sptr=s1;
-    else
-    *sptr=s2;
-    printf("\nMENU");
-    printf("\n 1. Adding. ");
-    printf("\n 2. Deleting. ");
-    printf("\n 3. Peek.");
-    printf("\n 4. Display all elements. ");
-    scanf("%d",&num);
-    if(num==1)
+    while(1)
     {
-        int flag1=isFull(sptr);
-        if(flag1==1)
-        printf("\n OVERLOAD!!");
+        printf("Enter the maximum number of elements for s1: \n");
+        scanf("%u",&s1.MAX);
+        printf("Enter the maximum number of elements for s2: \n");
+        scanf("%u",&s2.MAX);
+        printf("Enter the choice of stack: (1 or 2)\n");
+        scanf("%d",&stchoice);
+        if(stchoice==1)
+        sptr=&s1;
         else
+        sptr=&s2;
+        printf("MENU\n");
+        printf("1. Adding.\n ");
+        printf("2. Deleting.\n ");
+        printf("3. Peek.\n");
+        printf("4. Exit\n");
+        scanf("%d",&num);
+        if(num==1)
         {
-            struct student new;
-            printf("\n Enter roll no. of new student: ");
-            scanf("%d",&new);
-            push(sptr, new);
+            char flag1=isFull(sptr);
+            if(flag1==1)
+            printf("OVERLOAD!!\n");
+            else
+            {
+                struct student new;
+                printf("Enter roll no. of new student: \n");
+                scanf("%d",&new);
+                push(sptr, new);
+            }
+        }
+        else if(num==2)
+        {
+            char flag2=isEmpty(sptr);
+            if(flag2==1)
+            printf("\n UNDERFLOW!!");
+            else
+            {
+                pop(sptr);
+            }
+        }
+        else if(num==3)
+        {
+            peek(sptr);
+        }
+        else if(num==4)
+        {
+            return(0);
         }
     }
-    else if(num==2)
-    {
-        int flag2=isEmpty(sptr);
-        if(flag2==1)
-        printf("\n UNDERFLOW!!");
-        else
-        {
-            pop(sptr);
-        }
-    }
-    else if(num==3)
-    {
-        peek(sptr);
-    }
-    else if(num==4)
-    {
-
-    }
-
 }
 
 char isEmpty(struct stack *x)
@@ -77,7 +88,7 @@ char isEmpty(struct stack *x)
     else 
     return 0;
 }
-char isFull(struct student *x)
+char isFull(struct stack *x)
 {
     if((*x).top==((*x).MAX-1))
     return 1;
@@ -85,13 +96,11 @@ char isFull(struct student *x)
     return 0;
 }
 
-unsigned peek(struct student *x)
+unsigned peek(struct stack *x)
 {
     return (*x).top;
 }
-/*In the above function, last element of the list cannot be modified.
-To enable modification, try the below code in place of return (*x).top :
-return (*x).a[(*x).top] */
+
 
 void push(struct stack *x, struct student s)
 {
@@ -103,16 +112,4 @@ void pop(struct stack *x)
 {
     (*x).top--;
 }
-void display(struct stack *x)
-{
-    
-}
-/*Above function 'kills' the popped element. In case the caller 
-function wants to use the popped variable, only the copy can be
-returned and return type of the function is struct student. 
-Use the following code instead of     (*x).top--: 
-struct student temp;
-temp=(*x).a[(*x).top];
-(*x).top--;
-return temp;
-*/
+
